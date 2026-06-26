00bb54fc776f496ef8a0d81edf40bad76818f8c9
Sowmiya P <sowmiyaparthiban4@gmail.com>
2026-06-26 12:22:54 +0530

    Add files via upload

---
+++ structures & unions/union_behavior.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+union data
+{
+        int i;
+        float f;
+        char c;
+};
+int main()
+{
+        union data d;
+        d.i=10;
+        printf("integer=%d\n",d.i);
+        d.f=2.75;
+        printf("float=%.2f\n",d.f);
+        d.c='A';
+        printf("character=%c\n",d.c);
+        printf("integer=%d\n",d.i);
+        printf("float=%.2f\n",d.f);
+        printf("character=%c\n",d.c);
+        return 0;
+}
